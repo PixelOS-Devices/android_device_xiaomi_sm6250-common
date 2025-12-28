4e562c99794784eacb74b6f2aad7b569d162589d
Debayan Kar <kardebayan3@gmail.com>
2025-12-28 16:18:40 +0000

    sm6250-common: Inherit MiuiCamera
    Change-Id: I11c614c3524ebbfbf89a2a1ebc8dbdb158c9af4a

--- atoll.mk
+++ atoll.mk
@@ -61,6 +61,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
 
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera-miatoll/MiuiCamera.mk)
+
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 # DebugFS
